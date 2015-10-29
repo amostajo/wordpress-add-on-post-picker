0f161cf2e2e70c57ca8d28e92554d345dbfe1fdf
Alejandro Mostajo <amostajo@amsgames.com>
2015-10-28 23:00:52 -0600

    Refactoring.
    Refactoring.

--- src/controllers/PostController.php
+++ src/controllers/PostController.php
@@ -18,38 +18,38 @@ use Amostajo\LightweightMVC\Request;
  */
 class PostController extends Controller
 {
-    /**
-     * Displayes queried POSTS in JSON format.
-     * @since 1.0
-     */
+	/**
+	 * Displayes queried POSTS in JSON format.
+	 * @since 1.0
+	 */
 	public function json()
 	{
-        $filter = [
-            'post_type'         => Request::input( 'type', 'post' ),
-            'post_status'       => Request::input( 'status', 'publish' ),
-            'posts_per_page'    => Request::input( 'limit', 16 ),
-        ];
-        // Check for additional filter options
-        // Categories
-        $input = Request::input( 'categories', false );
-        if ( $input ) $filter['category_name'] = $input;
-        // Tags
-        $input = Request::input( 'tags', false );
-        if ( $input ) $filter['tag'] = $input;
-        // Tags
-        $input = Request::input( 'search', false );
-        if ( $input ) $filter['s'] = $input;
+		$filter = [
+			'post_type'      => Request::input( 'type', 'post' ),
+			'post_status'    => Request::input( 'status', 'publish' ),
+			'posts_per_page' => Request::input( 'limit', 16 ),
+		];
+		// Check for additional filter options
+		// Categories
+		$input = Request::input( 'categories', false );
+		if ( $input ) $filter['category_name'] = $input;
+		// Tags
+		$input = Request::input( 'tags', false );
+		if ( $input ) $filter['tag'] = $input;
+		// Tags
+		$input = Request::input( 'search', false );
+		if ( $input ) $filter['s'] = $input;
 
-        // Query
-        $query = new WP_Query( $filter );
-        $posts = array();
-        while ( $query->have_posts() ) {
-            $query->the_post();
-            $model = new Post();
-            $posts[] = $model->from_post( get_post() )->to_array();
-        }
-        // Display
-        header( 'Content-Type: application/json' );
+		// Query
+		$query = new WP_Query( $filter );
+		$posts = array();
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			$model = new Post();
+			$posts[] = $model->from_post( get_post() )->to_array();
+		}
+		// Display
+		header( 'Content-Type: application/json' );
 		echo json_encode($posts);
 		die;
 	}
