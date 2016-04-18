e5a3774aaf8dbaa1a872315259200d8bd96f9c0b
Alejandro Mostajo <amostajo@gmail.com>
2016-04-18 04:52:28 -0600

    PostController updated
    Added try and catch and logger for error tracking.

--- src/controllers/PostController.php
+++ src/controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace Amostajo\Wordpress\PostPickerAddon\Controllers;
 
 use WP_Query;
+use Exception;
 use Amostajo\WPPluginCore\Cache;
+use Amostajo\WPPluginCore\Log;
 use Amostajo\Wordpress\PostPickerAddon\Models\Post;
 use Amostajo\LightweightMVC\Collection;
 use Amostajo\LightweightMVC\Controller;
@@ -15,45 +17,50 @@ use Amostajo\LightweightMVC\Request;
  * @author Alejandro Mostajo
  * @license MIT
  * @package Amostajo\Wordpress\PostPickerAddon
- * @version 1.0
+ * @version 1.1
  */
 class PostController extends Controller
 {
 	/**
 	 * Displayes queried POSTS in JSON format.
 	 * @since 1.0
+	 * @since 1.1 Added try/catch and log.
 	 */
 	public function json()
 	{
-		$filter = [
-			'post_type'      => Request::input( 'type', 'post' ),
-			'post_status'    => Request::input( 'status', 'publish' ),
-			'posts_per_page' => Request::input( 'limit', 16 ),
-		];
-		// Check for additional filter options
-		// Categories
-		$input = Request::input( 'categories', false );
-		if ( $input ) $filter['category_name'] = $input;
-		// Tags
-		$input = Request::input( 'tags', false );
-		if ( $input ) $filter['tag'] = $input;
-		// Tags
-		$input = Request::input( 'search', false );
-		if ( $input ) $filter['s'] = $input;
-
-		// Query
-		$query = new WP_Query( $filter );
 		$posts = array();
-		while ( $query->have_posts() ) {
-			$query->the_post();
-			$posts[] = Cache::remember(
-				'addon_postpicker_post_' . get_the_ID(),
-				15,
-				function() {
-					$model = new Post();
-					return $model->from_post( get_post() )->to_array();
-				}
-			);
+		try {
+			$filter = [
+				'post_type'      => Request::input( 'type', 'post' ),
+				'post_status'    => Request::input( 'status', 'publish' ),
+				'posts_per_page' => Request::input( 'limit', 16 ),
+			];
+			// Check for additional filter options
+			// Categories
+			$input = Request::input( 'categories', false );
+			if ( $input ) $filter['category_name'] = $input;
+			// Tags
+			$input = Request::input( 'tags', false );
+			if ( $input ) $filter['tag'] = $input;
+			// Tags
+			$input = Request::input( 'search', false );
+			if ( $input ) $filter['s'] = $input;
+
+			// Query
+			$query = new WP_Query( $filter );
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$posts[] = Cache::remember(
+					'addon_postpicker_post_' . get_the_ID(),
+					15,
+					function() {
+						$model = new Post();
+						return $model->from_post( get_post() )->to_array();
+					}
+				);
+			}
+		} catch ( Exception $e ) {
+			Log::error( $e );
 		}
 		// Display
 		header( 'Content-Type: application/json' );
